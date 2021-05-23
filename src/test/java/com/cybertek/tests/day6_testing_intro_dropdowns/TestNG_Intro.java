cad07717da729bf1828841a8bc9cbcd2202af39d
VadymLysenko <vesioluy@hotmail.com>
2021-05-23 14:26:49 -0500

    day 6 testNG beforeMethod

--- src/test/java/com/cybertek/tests/day6_testing_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cybertek/tests/day6_testing_intro_dropdowns/TestNG_Intro.java
@@ -1,9 +1,14 @@
 package com.cybertek.tests.day6_testing_intro_dropdowns;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_Intro {
 
+    @BeforeMethod
+    public void setupMethod() {
+        System.out.println("---> before method is running...");
+    }
 
     @Test
     public void test1() {
