aeaa0c2cc2122e1336ae9097fb5289710dc93e71
VadymLysenko <vesioluy@hotmail.com>
2021-05-23 13:56:42 -0500

    day 6 testNG intro

---
+++ src/test/java/com/cybertek/tests/day6_testing_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,18 @@
+package com.cybertek.tests.day6_testing_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+
+
+    @Test
+    public void test1() {
+        System.out.println("Running test 1...");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Running test 2...");
+    }
+
+}
