b97f84c2254c8cdca3ac7482ea58732e014241f1
VadymLysenko <vesioluy@hotmail.com>
2021-05-30 14:48:04 -0500

    day 7 Iframe_Practise 1 iframe locator

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_widows/IframePractice.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_widows/IframePractice.java
@@ -4,6 +4,7 @@ import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -25,9 +26,24 @@ public class IframePractice {
     @Test
     public void iframe_test() {
 
+        //#1 - changing to iframe by index: we pass index number
+        //driver.switchTo().frame(0);
+
+        //#2 - locating web element to switch
+        //we find and locate < iframe> web element from the page
+
+        //#3 - if <iframe> tag has id or name attribute, we can pass its attribute value as a string into
+        //.switch().frame() method directly
+
+        //WebElement iframe = driver.findElement(By.xpath("//iframe[@class='tox-edit-area__iframe']"));
+        driver.switchTo().frame("mce_0_ifr");
+
+        //locating web element text
         WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p"));
 
         //4. Assert: “Your content goes here.” Text is displayed.
+
+        Assert.assertTrue(yourContentGoesHereText.isDisplayed());
         //5. Assert: “An iFrame containing the TinyMCE WYSIWYG Editor
     }
 }
