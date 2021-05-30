6ee19ff345a013a8f304b72fa4698c56572b3ac2
VadymLysenko <vesioluy@hotmail.com>
2021-05-30 15:51:30 -0500

    day 7 window practice 1 getWindowHandle

--- src/test/java/com/cybertek/tests/day7_alerts_iframes_widows/WindowHandlePractice.java
+++ src/test/java/com/cybertek/tests/day7_alerts_iframes_widows/WindowHandlePractice.java
@@ -1,7 +1,9 @@
 package com.cybertek.tests.day7_alerts_iframes_widows;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -20,6 +22,27 @@ public class WindowHandlePractice {
     @Test
     public void window_test () {
 
+        //4. Assert: Title is “Practice”
 
+        String actualTitleBeforeClick = driver.getTitle();
+        String expectedTitleBeforeClick = "Practice";
+
+        System.out.println("actualTitleBeforeClick = " + actualTitleBeforeClick);
+
+        //5. Click to: “Click Here” text
+
+        WebElement clickHereLink = driver.findElement(By.linkText("Click Here"));
+        clickHereLink.click();
+
+        String mainHandle = driver.getWindowHandle();
+
+        System.out.println("mainHandle = " + mainHandle);
+
+        String actualTitleAfterClick = driver.getTitle();
+
+        System.out.println(" actualTitleAfterClick = " + actualTitleAfterClick);
+
+        //6. Switch to new Window.
+        //7. Assert: Title is “New Window
     }
 }
