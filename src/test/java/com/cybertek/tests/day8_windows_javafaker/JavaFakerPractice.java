2a7495aca809a7619b1132955ca880eb39679a96
VadymLysenko <vesioluy@hotmail.com>
2021-06-02 21:01:13 -0500

    added chuckNorris faker

--- src/test/java/com/cybertek/tests/day8_windows_javafaker/JavaFakerPractice.java
+++ src/test/java/com/cybertek/tests/day8_windows_javafaker/JavaFakerPractice.java
@@ -8,5 +8,13 @@ public class JavaFakerPractice {
     @Test
     public void test1()  {
         Faker faker = new Faker();
+
+        String fullName = faker.name().fullName();
+        System.out.println("fullName = " + fullName);
+        String firstName = faker.name().firstName();
+        System.out.println("firstName = " + firstName);
+
+        String chuckNorris = faker.chuckNorris().fact();
+        System.out.println("chuckNorris = " + chuckNorris);
     }
 }
