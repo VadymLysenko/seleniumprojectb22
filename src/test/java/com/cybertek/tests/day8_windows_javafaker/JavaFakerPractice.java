fb855cc119db82ce4e3debcf3c90f9be6658642e
VadymLysenko <vesioluy@hotmail.com>
2021-06-02 20:54:08 -0500

    day 8 starts using JavFaker

---
+++ src/test/java/com/cybertek/tests/day8_windows_javafaker/JavaFakerPractice.java
@@ -0,0 +1,12 @@
+package com.cybertek.tests.day8_windows_javafaker;
+
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+
+public class JavaFakerPractice {
+
+    @Test
+    public void test1()  {
+        Faker faker = new Faker();
+    }
+}
---
+++ src/test/java/com/cybertek/tests/day8_windows_javafaker/MultipleWindowsPractice.java
@@ -0,0 +1,9 @@
+package com.cybertek.tests.day8_windows_javafaker;
+
+public class MultipleWindowsPractice {
+
+    public static void main(String[] args) {
+
+
+    }
+}
