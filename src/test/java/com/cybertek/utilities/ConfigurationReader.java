052e0200dfda8be02c87db863a3ef4cc2b63af30
VadymLysenko <vesioluy@hotmail.com>
2021-06-06 15:55:07 -0500

    day 9 - ConfigurationReader added IO exception

--- src/test/java/com/cybertek/utilities/ConfigurationReader.java
+++ src/test/java/com/cybertek/utilities/ConfigurationReader.java
@@ -1,5 +1,7 @@
 package com.cybertek.utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 // we will create reusable method that will
@@ -11,7 +13,16 @@ public class ConfigurationReader {
    private static Properties properties = new Properties();
 
    static {
-       System.out.println();
+       //#2 - Load the file info FileInputStream
+       try {
+           FileInputStream file = new FileInputStream("configuration.properties");
+
+       //#3 - load properties object with the file (conf.prop)
+       properties.load(file);
+
+       } catch (IOException  e) {
+           System.out.println("File not found in Configuration properties.");
+       }
    }
 
 }
