e0f58c53c41d81ad2ce52ab489be96230e398ebc
VadymLysenko <vesioluy@hotmail.com>
2021-06-06 13:39:25 -0500

    day 9 P1 test login to library

--- src/test/java/com/cybertek/tests/day9_properties_browser_utils/P1_Library_Login.java
+++ src/test/java/com/cybertek/tests/day9_properties_browser_utils/P1_Library_Login.java
@@ -1,19 +1,51 @@
 package com.cybertek.tests.day9_properties_browser_utils;
 
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class P1_Library_Login {
 
     WebDriver driver;
-    @Test
-    public void login_link_count_test() {
+
+    @BeforeClass
+    public void setupClass() {
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get("http://library2.cybertekschool.com/login.html");
+    }
+
+    public void loginToLibrary() {
+        WebElement inputUsername = driver.findElement(By.xpath("//input[@id='inputEmail']"));
+        inputUsername.sendKeys("student11@library");
+
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@id='inputPassword']"));
+        inputPassword.sendKeys("tScBPCUr");
+
+        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Sign in']"));
+        loginButton.click();
+    }
+    @Test
+    public void login_link_count_test() {
+        loginToLibrary();
+
+        BrowserUtils.sleep(2);
+        List<WebElement> allLinks = driver.findElements(By.xpath("//body//a"));
+        allLinks.size();
+        System.out.println("allLinks.size() = " + allLinks.size());
+
+        for (WebElement each : allLinks) {
+            System.out.println(each.getText());
+        }
+
+
     }
 }
