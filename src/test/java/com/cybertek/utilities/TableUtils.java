7990125b07f33b1fcc06f73876c3c819765f750a
VadymLysenko <vesioluy@hotmail.com>
2021-06-09 20:39:01 -0500

    added find elements

--- src/test/java/com/cybertek/utilities/TableUtils.java
+++ src/test/java/com/cybertek/utilities/TableUtils.java
@@ -1,8 +1,15 @@
 package com.cybertek.utilities;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class TableUtils {
 
-    public static void verifyOrder() {
+    public static void verifyOrder(WebDriver driver, String name) {
+
+        //locate all the names and store them in a List of WebElements
+        driver.findElements(By.xpath("//table[@id='table1']//tr/td[2]"));
+
 
     }
 }
