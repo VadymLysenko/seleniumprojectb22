a690a4e1ee6ac7b7524c6e90943984d591c32e02
VadymLysenko <vesioluy@hotmail.com>
2021-06-09 20:42:08 -0500

    added for each loop

--- src/test/java/com/cybertek/utilities/TableUtils.java
+++ src/test/java/com/cybertek/utilities/TableUtils.java
@@ -2,13 +2,24 @@ package com.cybertek.utilities;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class TableUtils {
 
     public static void verifyOrder(WebDriver driver, String name) {
 
         //locate all the names and store them in a List of WebElements
-        driver.findElements(By.xpath("//table[@id='table1']//tr/td[2]"));
+        List<WebElement> allNames = driver.findElements(By.xpath("//table[@id='table1']//tr/td[2]"));
+
+        //to introduce local variable
+        //mac: option+enter
+        //windows : alt+enter
+
+        for(WebElement each : allNames) {
+            System.out.println(each.getText());
+        }
 
 
     }
