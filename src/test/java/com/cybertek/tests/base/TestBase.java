86c7673ce94fbc03dcddc653d64b17ccb7c5c12c
VadymLysenko <vesioluy@hotmail.com>
2021-06-16 19:39:53 -0500

    day 11 TestBase

--- src/test/java/com/cybertek/tests/base/TestBase.java
+++ src/test/java/com/cybertek/tests/base/TestBase.java
@@ -3,6 +3,7 @@ package com.cybertek.tests.base;
 import com.cybertek.utilities.ConfigurationReader;
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
@@ -21,6 +22,10 @@ public abstract class TestBase {
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
 
+    }
+    @AfterMethod
+    public void tearDownMethod() {
+        driver.close();
     }
 
 }
