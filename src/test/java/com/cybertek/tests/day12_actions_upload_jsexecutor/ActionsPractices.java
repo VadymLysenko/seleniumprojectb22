73580d51d03998be349ffd577c62720420d577a0
VadymLysenko <vesioluy@hotmail.com>
2021-06-19 13:39:42 -0500

    day 12 ActionsPractices

---
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/ActionsPractices.java
@@ -0,0 +1,11 @@
+package com.cybertek.tests.day12_actions_upload_jsexecutor;
+
+import org.testng.annotations.Test;
+
+public class ActionsPractices {
+
+    @Test
+    public void tc15_actions_hover() {
+
+    }
+}
