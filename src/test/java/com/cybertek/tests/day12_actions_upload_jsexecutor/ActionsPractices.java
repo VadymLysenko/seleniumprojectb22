0de659ee3396ecb1ad4f550c4d14c0d884d1f6cd
VadymLysenko <vesioluy@hotmail.com>
2021-06-19 14:11:40 -0500

    day 12 ActionsPractices

--- src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/ActionsPractices.java
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/ActionsPractices.java
@@ -3,6 +3,7 @@ package com.cybertek.tests.day12_actions_upload_jsexecutor;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class ActionsPractices {
@@ -21,5 +22,10 @@ public class ActionsPractices {
         WebElement user1 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user1']"));
         WebElement user2 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user2']"));
         WebElement user3 = Driver.getDriver().findElement(By.xpath("//h5[.='name: user3']"));
+
+
+        //we must create object of actions class to use method coming from there
+        Actions actions = new Actions(Driver.getDriver());
+
     }
 }
