ab69d3d07acdca1a6d723d9554901eb96b48fac1
VadymLysenko <vesioluy@hotmail.com>
2021-06-19 13:47:09 -0500

    day 12 ActionsPractices

--- src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/ActionsPractices.java
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/ActionsPractices.java
@@ -1,5 +1,8 @@
 package com.cybertek.tests.day12_actions_upload_jsexecutor;
 
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class ActionsPractices {
@@ -7,5 +10,12 @@ public class ActionsPractices {
     @Test
     public void tc15_actions_hover() {
 
+
+        Driver.getDriver().get("http://practice.cybertekschool.com/hovers");
+
+        //locating all of the images here:
+        WebElement img1 = Driver.getDriver().findElement(By.xpath("(//img)[1]"));
+        WebElement img2 = Driver.getDriver().findElement(By.xpath("(//img)[2]"));
+        WebElement img3 = Driver.getDriver().findElement(By.xpath("(//img)[3]"));
     }
 }
