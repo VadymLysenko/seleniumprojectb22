6c3598e113caa37101b40fc3e70db8c64a5c6893
VadymLysenko <vesioluy@hotmail.com>
2021-06-19 09:39:28 -0500

    day 12 close Driver Utility Class

---
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/DriverUtilsPractice.java
@@ -0,0 +1,14 @@
+package com.cybertek.tests.day12_actions_upload_jsexecutor;
+
+import org.testng.annotations.Test;
+
+public class DriverUtilsPractice {
+
+    @Test
+    public void simple_google_search_test() {
+        //1-go to google.com
+        //2-search for a value
+        //3-verify value is contained in the title
+
+    }
+}
