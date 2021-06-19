464c7390c9ef3cd4ca4c071fa8e3a4b21ba7bdd3
VadymLysenko <vesioluy@hotmail.com>
2021-06-19 13:20:18 -0500

    day 12 UploadPractice

--- src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/UploadPractice.java
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/UploadPractice.java
@@ -1,6 +1,9 @@
 package com.cybertek.tests.day12_actions_upload_jsexecutor;
 
+import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class UploadPractice {
@@ -11,7 +14,16 @@ public class UploadPractice {
 
         String path = "/Users/waynelysenko/Desktop/pizzza.jpg";
 
+        WebElement chooseFile = Driver.getDriver().findElement(By.id("file-upload"));
 
+        WebElement uploadButton = Driver.getDriver().findElement(By.id("file-submit"));
+        //sending the file path to the chooseFile WebElement
+        BrowserUtils.sleep(3);
+
+        chooseFile.sendKeys(path);
+
+        //click to upload button
+        uploadButton.click();
     }
 
 }
