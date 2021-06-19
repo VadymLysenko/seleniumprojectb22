0aa41c1c3edae0bb9c7cbce41ee488a42edabb62
VadymLysenko <vesioluy@hotmail.com>
2021-06-19 15:57:47 -0500

    day 12 ActionsPractices

--- src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/JSExecutorPractices.java
+++ src/test/java/com/cybertek/tests/day12_actions_upload_jsexecutor/JSExecutorPractices.java
@@ -2,7 +2,9 @@ package com.cybertek.tests.day12_actions_upload_jsexecutor;
 
 import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class JSExecutorPractices {
@@ -27,6 +29,18 @@ public class JSExecutorPractices {
         //this would go 750 pixels up
         js.executeScript("window.scrollBy(0, -750)");
 
+
+    }
+    @Test
+    public void scroll_using_jsExecutor_test2() {
+        Driver.getDriver().get("http://practice.cybertekschool.com/large");
+
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        WebElement cybertekSchoolLink = Driver.getDriver().findElement(By.linkText("Cybertek School"));
+        WebElement homeLink = Driver.getDriver().findElement(By.linkText("Home"));
+
+        // use js.executor with different scrolling JavaScript function
+        js.executeScript("arguments[0].scrollIntoView(true)",cybertekSchoolLink);
 
     }
 }
