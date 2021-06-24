be8bd3197b4ed9e93e55088be82cb7e453a594a7
VadymLysenko <vesioluy@hotmail.com>
2021-06-23 19:17:26 -0500

    day 13 ActionsPractices

--- src/test/java/com/cybertek/tests/day13_actions_pom_design/ActionsPractices.java
+++ src/test/java/com/cybertek/tests/day13_actions_pom_design/ActionsPractices.java
@@ -1,6 +1,8 @@
 package com.cybertek.tests.day13_actions_pom_design;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 public class ActionsPractices {
@@ -8,10 +10,11 @@ public class ActionsPractices {
     @Test
     public void p2_double_click_test() {
         //1. Go to https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2
-
         Driver.getDriver().get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick2");
         //2. Switch to iframe.
+        Driver.getDriver().switchTo().frame("iframeResult");
         //3. Double click on the text “Double-click me to change my text color.”
+        WebElement textToDoubleClick = Driver.getDriver().findElement(By.id("demo"));
         //4. Assert: Text’s “style” attribute value contains “red”.
 
 
