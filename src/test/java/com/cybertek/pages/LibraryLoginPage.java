bd6e478befe70b73da45bf3c7bc3f71946449f4e
VadymLysenko <vesioluy@hotmail.com>
2021-06-23 19:49:53 -0500

    day 13 LibraryLoginPage

---
+++ src/test/java/com/cybertek/pages/LibraryLoginPage.java
@@ -0,0 +1,21 @@
+package com.cybertek.pages;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LibraryLoginPage {
+
+    //1 - create constructor and initialize driver and object of the class
+    public LibraryLoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "inputEmail")
+    public WebElement inputEmail;
+
+    @FindBy(id = "inputPassword")
+    public WebElement inputPassword;
+
+}
