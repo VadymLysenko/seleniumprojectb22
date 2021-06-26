d107b203132e31af625e43458fc761bdafaaf56e
VadymLysenko <vesioluy@hotmail.com>
2021-06-26 13:40:55 -0500

    day 14 pom_synchronization

--- src/test/java/com/cybertek/tests/day14_pom_synchronization/WebDriverWaitPractice.java
+++ src/test/java/com/cybertek/tests/day14_pom_synchronization/WebDriverWaitPractice.java
@@ -1,5 +1,6 @@
 package com.cybertek.tests.day14_pom_synchronization;
 
+import com.cybertek.pages.DynamicLoad1Page;
 import com.cybertek.pages.DynamicLoad7Page;
 import com.cybertek.utilities.Driver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -34,11 +35,24 @@ public class WebDriverWaitPractice {
         //1. Go to http://practice.cybertekschool.com/dynamic_loading/1
         Driver.getDriver().get("http://practice.cybertekschool.com/dynamic_loading/1");
 
+        DynamicLoad1Page dynamicLoad1Page = new DynamicLoad1Page();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+
         // 2. Click to start
+        dynamicLoad1Page.startButton.click();
+
         //3. Wait until loading bar disappears
+        wait.until(ExpectedConditions.invisibilityOf(dynamicLoad1Page.loadingBar));
+
         //4. Assert username inputbox is displayed
+        Assert.assertTrue(dynamicLoad1Page.inputUsername.isDisplayed());
+
         //5. Enter username: tomsmith
+        dynamicLoad1Page.inputUsername.sendKeys("tomsmith");
+
         //6. Enter password: incorrectpassword
+        dynamicLoad1Page.inputPassword.sendKeys("incorrectPassword");
+
         //7. Click to Submit button
         //8. Assert “Your password is invalid!” text is displayed.
         //Note: Follow POM Design Pattern
