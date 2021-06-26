83c4181efac036e3b6ed2bc36463002d0cb574a3
VadymLysenko <vesioluy@hotmail.com>
2021-06-26 11:59:26 -0500

    day 14 pom_synchronization

--- src/test/java/com/cybertek/tests/day14_pom_synchronization/WebDriverWaitPractice.java
+++ src/test/java/com/cybertek/tests/day14_pom_synchronization/WebDriverWaitPractice.java
@@ -1,5 +1,8 @@
 package com.cybertek.tests.day14_pom_synchronization;
 
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public class WebDriverWaitPractice {
@@ -7,7 +10,11 @@ public class WebDriverWaitPractice {
     @Test
     public void dynamic_loading_7_test() {
 
+        Driver.getDriver().get("http://practice.cybertekschool.com/dynamic_loading/7");
 
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
 
+        // use the object to wait for the explicit condition
+        wait.until(ExpectedConditions.titleIs("Dynamic title"));
     }
 }
