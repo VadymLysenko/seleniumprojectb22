9962c3d04e8b6594a5ab6081669ce3b0d56d7fad
VadymLysenko <vesioluy@hotmail.com>
2021-06-26 10:06:26 -0500

    day 14 pom_synchronization

---
+++ src/test/java/com/cybertek/tests/day14_pom_synchronization/ActionsPractices.java
@@ -0,0 +1,17 @@
+package com.cybertek.tests.day14_pom_synchronization;
+
+import org.testng.annotations.Test;
+
+public class ActionsPractices {
+
+    @Test
+    public void drag_and_drop_test() {
+        /**
+         * 1.Go to https://demos.telerik.com/kendo-ui/dragdrop/index
+         * 2.Drag and drop the small circle to bigger circle.
+         * 3.Assert: -Text in big circle changed to: “You did great!”
+         */
+
+
+    }
+}
