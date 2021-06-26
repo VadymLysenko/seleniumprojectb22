7c35f85ec55835d094bd68ab09f37090c65a2db1
VadymLysenko <vesioluy@hotmail.com>
2021-06-26 11:47:20 -0500

    day 14 pom_synchronization

---
+++ src/test/java/com/cybertek/tests/day14_pom_synchronization/WebDriverWaitPractice.java
@@ -0,0 +1,13 @@
+package com.cybertek.tests.day14_pom_synchronization;
+
+import org.testng.annotations.Test;
+
+public class WebDriverWaitPractice {
+
+    @Test
+    public void dynamic_loading_7_test() {
+
+
+
+    }
+}
