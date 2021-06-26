5b55c95766c2274a19ef8d4f58565c644478169b
VadymLysenko <vesioluy@hotmail.com>
2021-06-26 11:52:36 -0500

    day 14 pom_synchronization

--- src/test/java/com/cybertek/pages/DynamicLoad7Page.java
+++ src/test/java/com/cybertek/pages/DynamicLoad7Page.java
@@ -1,6 +1,8 @@
 package com.cybertek.pages;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DynamicLoad7Page {
@@ -8,4 +10,9 @@ public class DynamicLoad7Page {
     public DynamicLoad7Page() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
+    @FindBy(xpath = "//strong[.='Done!']")
+    public WebElement doneMessage;
+
+    @FindBy(xpath = "//img[@alt='square pants']")
+    public WebElement spongeBobImage;
 }
