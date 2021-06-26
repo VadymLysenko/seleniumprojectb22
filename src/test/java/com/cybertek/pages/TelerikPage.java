4c6f3339c93a28abbd0b4bb821526ff0ec8d8dc5
VadymLysenko <vesioluy@hotmail.com>
2021-06-26 10:13:49 -0500

    day 14 pom_synchronization

--- src/test/java/com/cybertek/pages/TelerikPage.java
+++ src/test/java/com/cybertek/pages/TelerikPage.java
@@ -1,6 +1,8 @@
 package com.cybertek.pages;
 
 import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class TelerikPage {
@@ -8,4 +10,9 @@ public class TelerikPage {
     public TelerikPage() {
         PageFactory.initElements(Driver.getDriver(),this);
     }
+    @FindBy(id = "draggable")
+    public WebElement smallCircle;
+
+    @FindBy(id = "droptarget")
+    public WebElement bigCircle;
 }
